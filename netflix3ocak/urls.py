1b34c724e52b0854e95ca0d51b2e482df590a238
Berkay Ülger <berkayulger45920@gmail.com>
2023-07-11 10:06:49 +0300

    -

---
+++ netflix3ocak/urls.py
@@ -0,0 +1,32 @@
+"""netflix3ocak URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from appMy.views import *
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', indexPage,name='indexPage'),
+    path('browsePage', browsePage, name='browsePage'),
+    path('profileUser', profileUser,name='profileUser'),
+    path('accountUser', accountUser,name='accountUser'),
+    path('loginUser',loginUser ,name='loginUser'),
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
